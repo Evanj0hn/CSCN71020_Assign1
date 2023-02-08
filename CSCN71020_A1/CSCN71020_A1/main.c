5531ecfafc45510fd920562cb2c13ac01ed9269f
Evanj0hn <122998608+Evanj0hn@users.noreply.github.com>
2023-02-07 23:21:49 -0500

    Fixed the addition operation

--- CSCN71020_A1/CSCN71020_A1/main.c
+++ CSCN71020_A1/CSCN71020_A1/main.c
@@ -3,7 +3,6 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-void subtract();
 
 
 void main() {
@@ -21,8 +20,6 @@ void main() {
 	{
 	case 1:
 		add();
-	case 2:
-		subtract();
 	}
 
 }
@@ -48,21 +45,5 @@ void add() {
 	printf("Enter the third value:");
 	scanf_s("%lf", &num3);
 	result = num1 + num2+num3;
-	printf("%lf + %lf + %lf = %lf\n", num1, num2,num3,result);
-
-	
-}
-
-void subtract() {
-	double num1, num2, num3, result;
-	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
-	printf("Enter the third value:");
-	scanf_s("%lf", &num3);
-	result = num1 - num2 - num3;
-	printf("%lf - %lf - %lf = %lf\n", num1, num2,num3,result);
-
-
+	printf("%lf + %lf = %lf\n", num1, num2, ,num3,result);
 }
